0776c3ba2201c929724b9481ae80c7a294c7a5dd
Bambangsolehudin <bamsolotaku@gmail.com>
2020-11-06 08:05:06 +0700

    migration travel packages,galerries,transaction,transaction detail.

---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,10 @@
+    <link
+    rel="stylesheet"
+    href="{{url('frontend/libraries/bootstrap/css/bootstrap.css')}}"
+    />
+    <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{url('frontend/styles/main.css')}}" />
+    <link
+    href="https://fonts.googleapis.com/css?family=Assistant&display=swap"
+    rel="stylesheet"
+    />
