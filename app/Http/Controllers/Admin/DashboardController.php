51385458b8781ef5d62d4cf736f8b34e8ca3ac76
Bambangsolehudin <bamsolotaku@gmail.com>
2020-11-06 19:38:58 +0700

    Tambah data Statistic dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\TravelPackageRequest;
 use Illuminate\Http\Request;
+use App\Transaction;
+use App\TransactionDetail;
+use App\TravelPackage;
+use App\Gallery;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.admin.dashboard');
+        
+        return view('pages.admin.dashboard',[
+            'travel_package' =>  TravelPackage::count(),
+            'gallery' =>  Gallery::count(),
+            'transaction' =>  Transaction::count(),
+            'transaction_pending' =>  Transaction::where('transaction_status', 'pending')->count(),
+            'transaction_success' =>  Transaction::where('transaction_status', 'success')->count(),
+        ]);
     }
 }
